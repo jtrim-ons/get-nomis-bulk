a213c7ba07d0103d4cf9a37b7687481547e0280e
James Trimble <jtrim.ons@gmail.com>
2021-10-21 14:33:52 +0100

    Give option to try again

--- make-recode-rules.sh
+++ make-recode-rules.sh
@@ -18,10 +18,17 @@ ls tidied-data | while read f; do
         read -r -p "Enter recoding rules (e.g. a=2+3 b=5+6 *=7):" categories < /dev/tty
         #cat < /dev/tty > tmp-categories.txt
         python3 recode.py $categories < tidied-data/$f > /dev/null || echo "*** Something went wrong when attempting to recode! ***"
+
         read -r -p "Are you happy with this?" response < /dev/tty
         if [[ "$response" =~ ^([yY]) ]]
         then
             echo "$f $categories" >> $rules_filename
+            break
+        fi
+
+        read -r -p "Try again?" response < /dev/tty
+        if [[ ! "$response" =~ ^([yY]) ]]
+        then
             break
         fi
         done
