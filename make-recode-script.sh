ee4c0cee53375fc9928bd02d274c6fc0bb4b830e
James Trimble <jtrim.ons@gmail.com>
2021-10-21 13:21:48 +0100

    Show results of recoding while making recode script

--- make-recode-script.sh
+++ make-recode-script.sh
@@ -17,6 +17,7 @@ ls tidied-data | head -n5 | while read f; do
         while true; do
         read -r -p "Enter recoding rules (e.g. a=2+3 b=5+6 *=7):" categories < /dev/tty
         #cat < /dev/tty > tmp-categories.txt
+        python3 recode.py $categories < tidied-data/$f > /dev/null || echo "*** Something went wrong when attempting to recode! ***"
         read -r -p "Are you happy with this?" response < /dev/tty
         if [[ "$response" =~ ^([yY]) ]]
         then
