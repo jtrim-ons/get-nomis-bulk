e9d839a8f83eddfb556aae41459d169c54bd1cf3
James Trimble <jtrim.ons@gmail.com>
2021-10-21 13:14:56 +0100

    Make recode script more verbose, and add error checking

--- recode.py
+++ recode.py
@@ -2,6 +2,7 @@ import csv
 import sys
 
 def make_rules(args):
+    # A special '*' rule is the index of a total column, for checking
     rules = []
     for arg in args:
         name, columns = arg.split('=')
@@ -12,11 +13,35 @@ def make_rules(args):
 def make_columns(rownum, row, rules):
     if rownum == 0:
         result = ['code']
-        result.extend([name for name, _ in rules])
-    else:
-        result = [row[0]]
+        columns_seen = set()
         for name, columns in rules:
-            result.append(sum(int(row[c]) for c in columns))
+            long_name = name
+            if name == "*":
+                long_name = "* (column used for checking total)"
+            sys.stderr.write('{}:\n'.format(long_name))
+            for col in columns:
+                columns_seen.add(col)
+                sys.stderr.write('    ({}) {}\n'.format(col + 1, row[col]))
+        sys.stderr.write('Unused columns:\n')
+        for i, col_name in enumerate(row):
+            if i == 0: continue
+            if i not in columns_seen:
+                sys.stderr.write('    ({}) {}\n'.format(i + 1, col_name))
+
+        result.extend([name for name, _ in rules if name != "*"])
+    else:
+        expected_total = None
+        result = [row[0]]
+        total = 0
+        for name, columns in rules:
+            val = sum(int(row[c]) for c in columns)
+            if name == "*":
+                expected_total = val
+            else:
+                total += val
+                result.append(val)
+        if expected_total is not None:
+            assert expected_total == total
     return result
 
 rules = make_rules(sys.argv[1:])
